c777bb386ef69eb3c6b15fea9eab7693bcc37905
Adolfo Jara <gavilanesadolfo@gmail.com>
2021-12-18 18:09:24 -0500

    Able to update profile infor

--- Clips-Flask-Vladi/practicar/forms.py
+++ Clips-Flask-Vladi/practicar/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf.form import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms.fields.simple import BooleanField, PasswordField, SubmitField, StringField
 from wtforms.validators import DataRequired, EqualTo, Length, Email, ValidationError
 from practicar.models import User 
+from flask_login import current_user
 
 
 class RegistrationForm(FlaskForm):
@@ -37,3 +39,27 @@ class LoginForm(FlaskForm):
         
     #To submit a form
     submit = SubmitField('Login')
+    
+    
+class UpdateAccountForm(FlaskForm):
+    
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    
+    picture = FileField('Update profile picture', validators=[FileAllowed(['jpg', 'jpeg', 'png'])])
+        
+    #To submit a form
+    submit = SubmitField('Update')
+    
+    #to validate that the fields are unique
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken , choose another one')
+            
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken , choose another one')
--- Clips-Flask-Vladi/practicar/routes.py
+++ Clips-Flask-Vladi/practicar/routes.py
@@ -2,9 +2,14 @@ from flask import render_template, url_for, flash, redirect, request
 from flask_bcrypt import check_password_hash
 from practicar.models import User, Post
 #to import the forms created in forms.py
-from practicar.forms import RegistrationForm, LoginForm
+from practicar.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from practicar import app, bcrypt, db
 from flask_login import login_user, current_user, logout_user, login_required
+
+import secrets
+import os 
+#to resize image
+from PIL import Image 
 
 posts = [
     {
@@ -79,9 +84,50 @@ def logout():
     return redirect(url_for('indice'))
 
 
-@app.route("/account")
+def savePicture(form_picture):
+    
+    #to produce a random name for the image so it doesnt repeat
+    random_hex = secrets.token_hex(8)
+    
+    _, f_ext = os.path.splitext(form_picture.filename)
+    
+    picture_fn = random_hex + f_ext
+    
+    picture_path = os.path.join(app.root_path, 'static/Pics', picture_fn)
+    
+    #resize image
+    output_size = (125,125)
+    img = Image.open(form_picture)
+    img.thumbnail(output_size)
+        
+    #save image
+    img.save(picture_path)
+        
+    return picture_fn
+    
+
+@app.route("/account", methods=['GET','POST'])
 @login_required
 def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        
+        if form.picture.data:
+            picture_file = savePicture(form.picture.data)
+            current_user.image_file = picture_file
+            
+        current_user.username = form.username.data 
+        current_user.email = form.email.data 
+        
+        db.session.commit()
+        flash('Your account has been updated', category="success")
+        return redirect(url_for('account'))
     
-    return render_template('account.html', title='Account')
+    #to put the user data in the form when is logged in
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    
+    image_file = url_for('static', filename="Pics/" + current_user.image_file)
+    return render_template('account.html', title='Account', image_file = image_file, form=form)
 
